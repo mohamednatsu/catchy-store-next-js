95f46be767066b6630a800a6176cfb9168e9724d
mohamednatsu <msalhnwr@gmail.com>
2024-11-14 22:32:53 +0200

    add arrows

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,10 +23,13 @@ function Navbar({open, handleOpen}) {
                 <Image className='cursor-pointer' src={logo} alt='logo' width={120} height={120} />
             </Link>
             </div>
-            <div className="w-1/2 flex justify-center items-center">
+            <div className="w-1/2 flex justify-center items-center relative">
                 <Link href={"/carts"}>
                     <HiOutlineShoppingBag color='white' className=' cursor-pointer hover:text-white/90 transition md:text-[33px] text-[25px]' />
                 </Link>
+                <div className="flex justify-center items-center md:translate-x-[-45px] p-2 translate-x-[-38px] translate-y-[12px] md:translate-y-[15px] w-[20px] h-[20px] rounded-full bg-gray-400/80 font-bold text-center">
+                    3
+                </div>
             </div>
         </div>
     )
